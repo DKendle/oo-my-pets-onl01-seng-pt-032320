a938c253021165d10c988933ab2ab5d336ac9f34
Dina Kendle <dina.kendle@gmail.com>
2020-04-24 13:52:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,9 +2,18 @@ class Dog
   attr_accessor :owner, :mood
   attr_reader :name
   
+  
+  
+  @@all_dogs = []
+  
   def initialize(name, owner)
     @name = name
     @owner = owner
     @mood = "nervous"
+    @@all_dogs << self 
+  end
+  
+  def self.all_dogs
+    @@all_dogs
   end
 end
